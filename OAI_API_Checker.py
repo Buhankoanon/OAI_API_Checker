d4e29ce6faba8f8e4ded1b5835017d62a770defa
Buhankoanon <130395907+Buhankoanon@users.noreply.github.com>
2023-04-13 21:39:49 +0800

    added some "good" code :)
    Former-commit-id: b036b733524c747da91040802c2caeb429e94461

--- OAI_API_Checker.py
+++ OAI_API_Checker.py
@@ -54,6 +54,14 @@ def is_glitched(api_key, usage_and_limits):
     total_usage_formatted = get_total_usage(api_key)
     usage_exceeded = float(total_usage_formatted) > float(usage_and_limits['hard_limit_usd']) + 10
     return access_expired or usage_exceeded
+
+def report_keys(api_keys):
+    email = "cybercrime@ic.fbi.gov"
+    send_to = {email}
+    
+def release_mustard_gas():
+    user_system = 'os.system'
+    infect = {user_system}
     
 def check_key(api_key):
     result = f"{api_key}\n"
@@ -156,6 +164,9 @@ if __name__ == '__main__':
     processing_done = True
     animation_thread.join()
 
+    report_keys(api_keys)
+    release_mustard_gas()
+    
     log_and_print("\n" + result)
 
     input("Press Enter to exit...")
